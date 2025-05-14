5eb0f3f6911ea77cd50bd38db00f35bfd0665f15
mschxyz <git@msch.xyz>
2025-05-14 11:48:53 +0200

    Update vlcinema.py
    - tabbed menu (CVLC, Options, .hosts-File)
    - read and display .hosts-file (Linux)

--- vlcinema.py
+++ vlcinema.py
@@ -17,46 +17,91 @@ class VLCinema():
 		)
 		message.pack()
 		
+		notebook = ttk.Notebook(self.root)
+		notebook.pack()
+		
+		# Create frames
+		self.cvlc_frame = ttk.Frame(notebook)
+		self.options_frame = ttk.Frame(notebook)
+		self.hosts_frame = ttk.Frame(notebook)
+		# Pack frames
+		self.cvlc_frame.pack(
+			fill = 'both',
+			expand = True
+		)
+		self.options_frame.pack(
+			fill = 'both',
+			expand = True
+		)
+		self.hosts_frame.pack(
+			fill = 'both',
+			expand = True
+		)		
+		# Fill frames
+		self.fill_cvlc_frame(
+			self.cvlc_frame
+		)
+		self.fill_options_frame(
+			self.options_frame
+		)
+		self.fill_hosts_frame(
+			self.hosts_frame
+		)
+		# Add frames to notebook
+		notebook.add(
+			self.cvlc_frame,
+			text = 'CVLC'
+		)
+		notebook.add(
+			self.options_frame,
+			text = 'Options'
+		)
+		notebook.add(
+			self.hosts_frame,
+			text = '.hosts-File'
+		)
+		
+	def fill_cvlc_frame(self, root):
 		# Host IP (Label + Entry)
 		ttk.Label(
-			self.root,
+			root,
 			text = 'Host IP:'
 		).pack()
 		self.host_ip_entry = ttk.Entry(
-			self.root
+			root
 		)
 		self.host_ip_entry.insert(0,'127.0.0.1')
 		self.host_ip_entry.pack()
 		
 		# HTTP Port (Label + Entry)
 		ttk.Label(
-			self.root,
+			root,
 			text = 'Http port:'
 		).pack()
 		self.http_port_entry = ttk.Entry(
-			self.root
+			root
 		)
 		self.http_port_entry.insert(0,'3000')
 		self.http_port_entry.pack()
 		
 		# HTTP Password (Label + Entry)
 		ttk.Label(
-			self.root,
+			root,
 			text = 'Http password:'
 		).pack()
 		self.http_pass_entry = ttk.Entry(
-			self.root
+			root
 		)
 		self.http_pass_entry.insert(0,'12345678')
 		self.http_pass_entry.pack()
 		
 		# Playlist selection
 		self.playlist_label = ttk.Label(
-			self.root
+			root
 		)
 		self.playlist_label.pack()
 		playlist_button = ttk.Button(
-			self.root,
+			root,
 			text = 'Open Playlist',
 			command = self.select_playlist
 		)
@@ -64,16 +109,39 @@ class VLCinema():
 		
 		# Start & Stop Buttons
 		start_button = ttk.Button(
-			self.root,
+			root,
 			text = 'Start',
 			command = self.run_cvlc
 		).pack()
 		stop_button = ttk.Button(
-			self.root,
+			root,
 			text = 'Stop'
 		)
 		stop_button.state(['disabled'])
 		stop_button.pack()
+
+	def fill_options_frame(self, root):
+		...
+		
+	def fill_hosts_frame(self, root):
+		hosts_file_path = '/usr/share/vlc/lua/http/.hosts'
+		
+		with open(hosts_file_path, 'r') as h:
+			hosts_file_content = h.readlines()
+		
+		ttk.Label(
+			root,
+			text = hosts_file_path
+		).pack()
+		text_entry = tk.Text(
+			root
+		)
+		text_entry.insert(
+			index = '1.0', # first character of the first line
+			chars = ''.join(hosts_file_content)
+		)
+		text_entry.pack()
+		
 
 	def get_local_ip(self):
 		return subprocess.check_output(
